d02df4ef03ba2419ea9ca425cb2c7e149b30e75d
liyongqiang <1240965061@qq.com>
2018-08-30 23:14:08 +0800

    ..

--- wbs/middlewares.py
+++ wbs/middlewares.py
@@ -8,7 +8,6 @@ import random
 from scrapy import signals
 from wbs import cookies
 
-
 class WbsSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
